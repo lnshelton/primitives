2494f4ff9c7a551eb6671fe3a0a11748f5b9fc2d
LaTonya Shelton <lnshelton32@gmail.com>
2014-07-08 14:41:46 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var a= 3;
+var b=2;
+var c=a+b;
+
+console.log ("this a string");
+console.log (a); 
+console.log (b); 
+console.log (c); 
+
+
+var first='sally';
+var last='smith':
+var fn='first' + 'last';
+
+console.log= "first" + "smith";
+
+
+//this is a itch statement
+//
+var color = 'blue';
